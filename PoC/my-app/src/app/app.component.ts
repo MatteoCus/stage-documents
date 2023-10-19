f8ac1c4a7a06a7e5e079bff9dc0af4df0fbad82e
MatteoCus <matteo.cusin58@gmail.com>
2023-10-19 16:52:51 +0200

    Updated token

--- PoC/my-app/src/app/app.component.ts
+++ PoC/my-app/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     window.addEventListener("load", () => {
       var x = document.getElementById("frame") as any;
         x!.contentWindow.postMessage({
-          token: "eyJjcmVhdGVkIjoxNjk3NTQ0NzY4NDIyLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.UyYEGssYcjLjvPiWb7ZjGRWHZuUMTdKLmkzx8DcApziFUcmDs35vVwqTzEI0g6hmCDG_fEPIbSRErfvZXFGQiw",
+          token: "eyJjcmVhdGVkIjoxNjk3NzI2NDUxMDAwLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.WFm1OqIBsZ4a0nX_SC882CZcZ9fKGMEW4dyC3yDLd8Huj98jGJxkCGXbJyp14nph1RiGS5tjo_Y8BXnqLHy7Gg",
           lang: "it",
           theme: "DM",
           username: "Simone Brazzarola",
