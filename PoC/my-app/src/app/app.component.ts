e0b603ca395c718a0690e8c3a0897c468d79b401
MatteoCus <matteo.cusin58@gmail.com>
2023-10-30 13:02:43 +0100

    Updated UM

--- PoC/my-app/src/app/app.component.ts
+++ PoC/my-app/src/app/app.component.ts
@@ -14,12 +14,12 @@ export class AppComponent {
     window.addEventListener("load", () => {
       var x = document.getElementById("frame") as any;
         x!.contentWindow.postMessage({
-          token: "eyJjcmVhdGVkIjoxNjk4MjQwNDA5ODM4LCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.KOFSX10aw64sUzA4MPVwgxuPuKLFaykK3aQrAM1iiZTq_u6JsDCueNE4ITmbRzHHVSRF7J5ZEY_P2lGjnpONMg",
+          token: "eyJjcmVhdGVkIjoxNjk4Mzk5MTg2ODI3LCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.rtagiL3YwOtgkM9ZVmvYUGBi27kgZX-PqiA9vEvrf_-9bhs-pGzbYPWF3yZ64xXDMtO8DYTc_EKgamhL14qb6w",
           lang: "it",
           theme: "DM",
           username: "Simone Brazzarola",
           m_product_id: 9013130,
-          c_projectphase_id: 9144672,
+          c_projectphase_id: 9144672, 
           user_id: 9000038
         }, "*");
       });
