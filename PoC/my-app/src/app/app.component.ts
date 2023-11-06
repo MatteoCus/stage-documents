c82bc13160bcc3cee4d94d37ad6addbeb151a412
MatteoCus <matteo.cusin58@gmail.com>
2023-11-06 10:00:10 +0100

    Updated iframe app token, ended requirements analysis

--- PoC/my-app/src/app/app.component.ts
+++ PoC/my-app/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     window.addEventListener("load", () => {
       var x = document.getElementById("frame") as any;
         x!.contentWindow.postMessage({
-          token: "eyJjcmVhdGVkIjoxNjk4Mzk5MTg2ODI3LCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.rtagiL3YwOtgkM9ZVmvYUGBi27kgZX-PqiA9vEvrf_-9bhs-pGzbYPWF3yZ64xXDMtO8DYTc_EKgamhL14qb6w",
+          token: "eyJjcmVhdGVkIjoxNjk5MjU1ODg0MzQwLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.Cny4dkuWKgxky-gP7y68ckJRwAZZq6n813KI7IEKvE0GEGQvAqkEV2jthvcgfTwx3MOdK1GpzPhc2iN8wtXhKg",
           lang: "it",
           theme: "DM",
           username: "Simone Brazzarola",
@@ -23,5 +23,42 @@ export class AppComponent {
           user_id: 9000038
         }, "*");
       });
+  }
+
+  public changePhase(phase: string): void {
+    switch(phase) {
+      case 'first':
+
+      var x = document.getElementById("frame") as any;
+        x!.contentWindow.postMessage({
+          token: "eyJjcmVhdGVkIjoxNjk5MjU1ODg0MzQwLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.Cny4dkuWKgxky-gP7y68ckJRwAZZq6n813KI7IEKvE0GEGQvAqkEV2jthvcgfTwx3MOdK1GpzPhc2iN8wtXhKg",
+          lang: "it",
+          theme: "DM",
+          username: "Simone Brazzarola",
+          m_product_id: 9009381,
+          c_projectphase_id: 9144611, 
+          user_id: 9000038
+        }, "*");
+
+      break;
+
+      case 'second':
+
+      var x = document.getElementById("frame") as any;
+        x!.contentWindow.postMessage({
+          token: "eyJjcmVhdGVkIjoxNjk5MjU1ODg0MzQwLCJhbGciOiJIUzUxMiJ9.eyJ1c2VySWQiOiI5MDAwMDY4IiwidXNlcm5hbWUiOiJNYXR0ZW9DIiwiYWRfY2xpZW50X2lkIjoiOTAwMDAwMCIsImFkX29yZ19pZCI6IjAiLCJhZF9yb2xlX2lkIjoiOTAwMDAwMSIsImFkX2xhbmd1YWdlX2lkIjoiaXRfSVQiLCJhdXRob3JpemF0aW9ucyI6W119.Cny4dkuWKgxky-gP7y68ckJRwAZZq6n813KI7IEKvE0GEGQvAqkEV2jthvcgfTwx3MOdK1GpzPhc2iN8wtXhKg",
+          lang: "it",
+          theme: "DM",
+          username: "Simone Brazzarola",
+          m_product_id: 9009381,
+          c_projectphase_id: 9144612, 
+          user_id: 9000038
+        }, "*");
+
+      break;
+
+      default:
+        break;
+    }
   }
 }
